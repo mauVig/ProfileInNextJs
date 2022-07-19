3726016d1b0bce606498534b9a0f19cba743f5d5
loesen-maurovigliero <91024327+mauVig@users.noreply.github.com>
2022-07-19 18:49:52 -0300

    change of logic in parallax

--- components/ParallaxAll.js
+++ components/ParallaxAll.js
@@ -1,19 +1,30 @@
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 import { Parallax } from 'react-scroll-parallax';
-import logipartes from '../public/img/logipartes.jpg';
-import dante from '../public/img/dante.jpg';
+// import logipartes from '../public/img/logipartes.jpg';
+// import dante from '../public/img/dante.jpg';
 
 import st from '../styles/details.module.css'
 
 export default function ParallaxLoesen({ name }) {
-  const img = name === 'dante'      ? dante : '' || 
-              name === 'logipartes' ? logipartes : '';
+  // const img = name === 'dante'      ? dante : '' || 
+  //             name === 'logipartes' ? logipartes : '';
+  const [who, setWho] = useState();
+
+  const dante = '/img/dante.jpg'
+  const matias = '/img/logipartes.jpg'
+
+
+  useEffect(()=>{
+    if (name === 'dante' ) setWho(dante)
+    if (name === 'logipartes' ) setWho(matias)
+  },[])
   return (
     <>
       <div className={`overflow-hidden ${st.diagonal}`}>
         <Parallax speed={-90}>
           <div className={`h-screen -z-10 `}>
-            <Image src={img} alt='Logipartes' layout='fill' />
+            <Image src={who} alt='Logipartes' layout='fill' />
           </div>
         </Parallax>
       </div>
